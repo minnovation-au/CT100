f5cb3f7ea0677c2dca44d829c8bde673b42901b0
M innovation <support@minnovation.com.au>
2018-04-04 08:16:08 +1000

    V1.5.1
    LoRa reply no longer waits with Delay. Instead uses Chrono() to
    Set wait period. When Chrono() is greater then 10 seconds it resends
    the original message.

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import machine
-from machine import deepsleep, Pin
+from machine import deepsleep, Pin, Timer
 import utime, ubinascii, socket, pycom
 from network import LoRa
 
 pycom.heartbeat(False)
-pycom.rgbled(0x007f00) # yellow
+pycom.rgbled(0x007f00) # Turn on Green LED
 
 pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
+chrono = Timer.Chrono()
 data = '0'
 
 lora = LoRa(mode=LoRa.LORA, region=LoRa.AU915, public=False)
-s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+sl = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 utime.sleep_ms(100)
 
 loramac = ubinascii.hexlify(lora.mac())
 
-while (data != loramac):
-
-    s.setblocking(True)
-    s.send(loramac+b'1'+b'{"val":1}')
-    utime.sleep_ms(100)
-    # wait for reply message
-    s.setblocking(False)
-    data = s.recv(64)
+def LoRaSend(y,val):
+    sl.setblocking(True)
+    sl.send(loramac+str(y)+val) # Send on LoRa Network & wait Reply
+    sl.setblocking(False)
+    chrono.reset()
+    print("Waiting for Reply")
+    while (sl.recv(17) != loramac+str(y)):
+        if (chrono.read() > 10):
+            print('Resubmit Packet')
+            pycom.rgbled(0x7f0000) # Turn on Red LED
+            sl.setblocking(True)
+            sl.send(loramac+str(y)+val)
+            sl.setblocking(False)
+            chrono.reset()
+print("Sending Packet")
+LoRaSend(1,'{"val":1}')
 
 while (pin() == 1):
     print("Waiting to Reset")
@@ -31,4 +40,3 @@ while (pin() == 1):
 print("Going to Sleep")
 machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
 machine.deepsleep()
-#utime.sleep_ms(1000)
