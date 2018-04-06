5f6dd7307bd9e3ea4bf4f744494357c80d9ba53a
M innovation <support@minnovation.com.au>
2018-04-06 11:53:47 +1000

    V1.5.3
    Updated LoRaID to ID

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import machine
+import machine, utime, ubinascii, socket, pycom
 from machine import deepsleep, Pin, Timer
-import utime, ubinascii, socket, pycom
 from network import LoRa
 
 pycom.heartbeat(False)
@@ -9,35 +8,34 @@ pycom.rgbled(0x007f00) # Turn on Green LED
 pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
 chrono = Timer.Chrono()
 chrono.start()
-data = '0'
 
 lora = LoRa(mode=LoRa.LORA, region=LoRa.AU915, public=False)
 sl = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 utime.sleep_ms(100)
 
-loramac = ubinascii.hexlify(lora.mac())
+def mac():
+    mac=ubinascii.hexlify(machine.unique_id(),':').decode()
+    mac=mac.replace(":","")
+    return(mac)
 
 def LoRaSend(y,val):
     sl.setblocking(True)
-    sl.send(loramac+str(y)+val) # Send on LoRa Network & wait Reply
+    sl.send(mac()+y+val) # Send on LoRa Network & wait Reply
     sl.setblocking(False)
     chrono.reset()
-    print("Waiting for Reply")
-    while (sl.recv(17) != loramac+str(y)):
+    while (sl.recv(13).decode('utf-8') != mac()+y):
         if (chrono.read() > 10):
             print('Resubmit Packet')
             pycom.rgbled(0x7f0000) # Turn on Red LED
             sl.setblocking(True)
-            sl.send(loramac+str(y)+val)
+            sl.send(mac()+y+val)
             sl.setblocking(False)
             chrono.reset()
-print("Sending Packet")
-LoRaSend(1,'{"val":1}')
+LoRaSend('1','{"val":1}')
 
 while (pin() == 1):
-    print("Waiting to Reset")
     utime.sleep_ms(1000)
 
-print("Going to Sleep")
+pycom.rgbled(0) # Turn OFF LED
 machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
 machine.deepsleep()
