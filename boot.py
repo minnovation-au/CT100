b1bc336ee6a7c2a63c1844c73efcbff488d1dec9
M innovation <support@minnovation.com.au>
2018-04-22 20:49:33 +1000

    Added WLAN Off

--- boot.py
+++ boot.py
@@ -7,7 +7,6 @@ key = b'alphaxencryptkeysjtwbutfdchdlcec' # 256 bit (32 bytes) key
 
 import socket, os, pycom, crypto, socket, gc
 from machine import SD, WDT
-from network import LoRa, WLAN
 from crypto import AES
 
 iv = crypto.getrandbits(128) # hardware generated random IV (never reuse it)
@@ -18,7 +17,9 @@ wdt = WDT(timeout=30000)
 pycom.heartbeat(False)
 pycom.rgbled(0x007f00) # Turn on Green LED
 
+################# WLAN ###################
 # Turn off WiFi to save power
+from network import WLAN
 w = WLAN()
 w.deinit()
 
