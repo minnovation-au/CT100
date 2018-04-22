4cbc5e5d7444ab3f0b4b7cd7dd78060d797bfd04
M innovation <support@minnovation.com.au>
2018-04-22 20:39:54 +1000

    V3.0
    Completely Re-Written

--- main.py
+++ main.py
@@ -1,114 +1,94 @@
-import machine, utime, ubinascii, socket, pycom, os
-from machine import deepsleep, Pin, Timer, SD, RTC
-from network import LoRa
+import machine, utime, ubinascii
+from machine import deepsleep, Pin, Timer
 
-rtc = RTC()
-#rtc.init((2018, 4, 14, 16, 59, 0, 0, 0))
-print(machine.wake_reason()[0])
-
-dateTime = str(utime.gmtime()[0])+"/"+str(utime.gmtime()[1])+"/"+str(utime.gmtime()[2])+" "+str(utime.gmtime()[3])+":00:00"
-
-pycom.heartbeat(False)
-pycom.rgbled(0x007f00) # Turn on Green LED
-
-pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
 chrono = Timer.Chrono()
 chrono.start()
-status=False
-lines=""
-val=""
+
+pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
+
+def aggregate():
+    try:
+        pycom.nvs_set('val', int(pycom.nvs_get('val'))+1)
+    except:
+        pycom.nvs_set('val', 0)
+    print('Deepsleep Remaining: '+str(machine.remaining_sleep_time()))
+    print('New Aggregate: '+str(pycom.nvs_get('val')))
+    print('Current Message ID: '+str(pycom.nvs_get('msgID')))
+
+def ammend_to_file():
+    try:
+        f = open('/flash/log.txt', 'r')
+        lines = f.readlines()
+        f.close()
+        lines=lines[1:]
+        #print(lines[-20:-1])
+        f = open('/flash/log.txt', 'w')
+        for a in range (0,999):
+            try:
+                f.write(lines[a])
+            except:
+                f.write('\n')
+        f.close()
+    except:
+        pass
+    f = open('/flash/log.txt', 'a')
+    f.write(str(pycom.nvs_get('msgID')))
+    f.write(',')
+    f.write(str(pycom.nvs_get('val')))
+    f.write('\n')
+    f.close()
 
 def mac():
     mac=ubinascii.hexlify(machine.unique_id(),':').decode()
     mac=mac.replace(":","")
     return(mac)
 
-def LoRaSend(y,val):
+def encrypt(send_pkg):
+    cipher = AES(key, AES.MODE_CFB, iv)
+    send_pkg = iv + cipher.encrypt(send_pkg)
+    return(send_pkg)
+
+def decrypt(recv_pkg):
+    cipher = AES(key, AES.MODE_CFB, recv_pkg[:16]) # on the decryption side
+    recv_pkg = cipher.decrypt(recv_pkg[16:])
+    return(recv_pkg)
+
+def LoRaSend(val):
     sl.setblocking(True)
-    sl.send(mac()+y+val) # Send on LoRa Network & wait Reply
-    print(mac()+y+val)
+    sl.send(encrypt(SITE+mac()+'&'+val)) # Send on LoRa Network & wait Reply
     sl.setblocking(False)
     chrono.reset()
-    c=0
-    while (sl.recv(13).decode('utf-8') != mac()+y):
+    recv_pkg=''
+    while (recv_pkg != mac()):
+        recv_pkg = sl.recv(48)
+        if (len(recv_pkg) > 2):
+            recv_pkg = decrypt(recv_pkg).decode('utf-8')
         if (chrono.read_ms() > 2000):
-            print('Resubmit Packet')
-            sl.setblocking(True)
-            sl.send(mac()+y+val)
-            sl.setblocking(False)
-            chrono.reset()
-            pycom.rgbled(0x00007f)
-            c = c+1
-            if (c == 10):
-                print('Packet Dropped')
-                return(False)
-    return(True)
+            print('Packet Dropped')
+            machine.deepsleep(60000)
+            return
+    print('Send Success')
+    ammend_to_file()
+    pycom.nvs_set('val', 0)
+    try:
+        pycom.nvs_set('msgID', int(pycom.nvs_get('msgID'))+1)
+    except:
+        pycom.nvs_set('msgID', 0)
 
-def openLog():
-    f = open('/sd/syslog.bin')
-    lines = f.readlines()
-    f.close()
-    return(lines)
-
-print (openLog()[0].split(',')[0])
-print (openLog()[0].split(',')[1])
-print (openLog())
-
-if openLog()[0].split(',')[0] == dateTime:
-  lines = openLog()[1:]
-  if machine.wake_reason()[0] == 1:
-      val = int(openLog()[0].split(',')[1])+1
-  else:
-      try:
-          val = int(openLog()[0].split(',')[1])
-      except:
-          val=0
-  print(openLog()[0].split(',')[2].strip('\n'))
-  if str(openLog()[0].split(',')[2].strip('\n')) == 'False':
-      try:
-          status = LoRaSend('1','{"val":'+str(openLog()[1].split(',')[1])+',"timestamp":"'+str(openLog()[1].split(',')[0])+'"}')
-      except:
-          pycom.rgbled(0x007f00)
-          status = False
-else:
-    lines = openLog()[:-1]
-    if machine.wake_reason()[0] == 1:
-        val = 1
-    else:
-        val = 0
-    status = LoRaSend('1','{"val":'+str(openLog()[1].split(',')[1].strip('\n'))+',"timestamp":"'+str(openLog()[1].split(',')[0])+'"}')
-
-def writeLog(lines,val,status):
-    f = open('/sd/syslog.bin','w')
-    f.write(str(dateTime))
-    f.write(',')
-    f.write(str(val))
-    f.write(',')
-    f.write(str(status))
-    f.write('\n')
-    for x in range(0,10):
-      try:
-        f.write(lines[x])
-      except:
-        f.write('\n')
-    f.close()
-
-writeLog(lines,val,status)
-
-#print(LoRaSend('1','{"val":1}'))
-
-b=0
-while (pin() == 1):
-    if b==30:
-        pycom.rgbled(0x00007f)
-        utime.sleep_ms(10)
-        pycom.rgbled(0)
-        b=0
-        print('waiting for pressure')
-    utime.sleep_ms(100)
-    b=b+1
-
+def paused():
+    while (pin() == 1):
+        utime.sleep_ms(100)
 
 pycom.rgbled(0) # Turn OFF LED
-machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
-machine.deepsleep(3590000)
+
+if machine.wake_reason()[0] == 1:
+    aggregate()
+    print('Aggregate MSGID: '+pycom.nvs_get('msgID')+'   Aggregate Value: '+pycom.nvs_get('val'))
+    paused()
+    machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
+    machine.deepsleep(machine.remaining_sleep_time()-chrono.read())
+else:
+    LoRaSend('{"val":'+str(pycom.nvs_get('val'))+',"msgID":'+str(pycom.nvs_get('msgID'))+'}')
+    print('Send: {"val":'+str(pycom.nvs_get('val'))+',"msgID":'+str(pycom.nvs_get('msgID'))+'}')
+    machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
+    machine.deepsleep(3600000)
