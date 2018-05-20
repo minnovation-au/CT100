b4d53ad0345cf7694c7b26980a86bbee199cbc90
Simon Maselli <simon.maselli@minnovation.com.au>
2018-05-20 19:10:33 +1000

    Removed Handshake

--- boot.py
+++ boot.py
@@ -1,21 +1,21 @@
 ################## Start setup (EDIT START HERE)
 
-SITE = 'SRCL'
-key = b'alphaxencryptkeysjtwbutfdchdlcec' # 256 bit (32 bytes) key
+#SITE = 'SRCL'
+#key = b'alphaxencryptkeysjtwbutfdchdlcec' # 256 bit (32 bytes) key
 
 ################## End setup (EDIT END HERE)
 
-import socket, os, pycom, crypto, socket, gc
-from machine import SD, WDT
-from crypto import AES
+#import socket, os, pycom, crypto, socket, gc
+#from machine import SD, WDT
+#from crypto import AES
 
-iv = crypto.getrandbits(128) # hardware generated random IV (never reuse it)
+#iv = crypto.getrandbits(128) # hardware generated random IV (never reuse it)
 
-gc.enable()
-wdt = WDT(timeout=30000)
+#gc.enable()
+#wdt = WDT(timeout=30000)
 
-pycom.heartbeat(False)
-pycom.rgbled(0x007f00) # Turn on Green LED
+#pycom.heartbeat(False)
+#pycom.rgbled(0x007f00) # Turn on Green LED
 
 ################# WLAN ###################
 # Turn off WiFi to save power
@@ -30,10 +30,10 @@ w.deinit()
 #ss.setblocking(True)
 
 ################# LORA #####################
-from network import LoRa
-lora = LoRa(mode=LoRa.LORA, region=LoRa.AU915)
-sl = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-sl.setblocking(False)
+#from network import LoRa
+#lora = LoRa(mode=LoRa.LORA, region=LoRa.AU915)
+#sl = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+#sl.setblocking(False)
 
 sd = SD()
 
--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-import machine, utime, ubinascii
+################## Start setup (EDIT START HERE)
+
+SITE = 'SRCL'
+key = b'alphaxencryptkeysjtwbutfdchdlcec' # 256 bit (32 bytes) key
+
+################## End setup (EDIT END HERE)
+
+import socket, os, pycom, crypto, socket, gc, machine
+from machine import SD, WDT
+from network import LoRa, WLAN
+from crypto import AES
+
+print(machine.wake_reason())
+
+iv = crypto.getrandbits(128) # hardware generated random IV (never reuse it)
+
+gc.enable()
+wdt = WDT(timeout=4000000)
+
+pycom.heartbeat(False)
+pycom.rgbled(0x007f00) # Turn on Green LED
+
+# Turn off WiFi to save power
+w = WLAN()
+w.deinit()
+
+lora = LoRa(mode=LoRa.LORA, region=LoRa.AU915, power_mode=LoRa.TX_ONLY)
+sl = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+import machine, utime, ubinascii, pycom
 from machine import deepsleep, Pin, Timer
 
 chrono = Timer.Chrono()
 chrono.start()
 
 pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
+
+pycom.heartbeat(False)
+pycom.rgbled(0x007f00) # Turn on Green LED
 
 def aggregate():
     try:
@@ -21,7 +53,7 @@ def ammend_to_file():
         lines = f.readlines()
         f.close()
         lines=lines[1:]
-        #print(lines[-20:-1])
+        print(lines[-20:-1])
         f = open('/flash/log.txt', 'w')
         for a in range (0,999):
             try:
@@ -42,6 +74,7 @@ def mac():
     mac=ubinascii.hexlify(machine.unique_id(),':').decode()
     mac=mac.replace(":","")
     return(mac)
+print(mac())
 
 def encrypt(send_pkg):
     cipher = AES(key, AES.MODE_CFB, iv)
@@ -57,17 +90,18 @@ def LoRaSend(val):
     sl.setblocking(True)
     sl.send(encrypt(SITE+mac()+'&'+val)) # Send on LoRa Network & wait Reply
     sl.setblocking(False)
-    chrono.reset()
-    recv_pkg=''
-    while (recv_pkg != mac()):
-        recv_pkg = sl.recv(48)
-        if (len(recv_pkg) > 2):
-            recv_pkg = decrypt(recv_pkg).decode('utf-8')
-        if (chrono.read_ms() > 2000):
-            print('Packet Dropped')
-            machine.deepsleep(60000)
-            return
-    print('Send Success')
+    #chrono.reset()
+    #recv_pkg=''
+    #while (recv_pkg != mac()):
+        #recv_pkg = sl.recv(48)
+        #if (len(recv_pkg) > 2):
+            #recv_pkg = decrypt(recv_pkg).decode('utf-8')
+        #if (chrono.read_ms() > 2000):
+            #print('Packet Dropped')
+            #machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
+            #machine.deepsleep(600000)
+            #return
+    #print('Send Success')
     ammend_to_file()
     pycom.nvs_set('val', 0)
     try:
@@ -83,12 +117,14 @@ pycom.rgbled(0) # Turn OFF LED
 
 if machine.wake_reason()[0] == 1:
     aggregate()
-    print('Aggregate MSGID: '+pycom.nvs_get('msgID')+'   Aggregate Value: '+pycom.nvs_get('val'))
+    print('Aggregate MSGID: '+str(pycom.nvs_get('msgID'))+'   Aggregate Value: '+str(pycom.nvs_get('val')))
     paused()
+    wdt.feed()
     machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
-    machine.deepsleep(machine.remaining_sleep_time()-chrono.read())
+    machine.deepsleep(machine.remaining_sleep_time())
 else:
-    LoRaSend('{"val":'+str(pycom.nvs_get('val'))+',"msgID":'+str(pycom.nvs_get('msgID'))+'}')
     print('Send: {"val":'+str(pycom.nvs_get('val'))+',"msgID":'+str(pycom.nvs_get('msgID'))+'}')
+    LoRaSend('{"val":'+str(pycom.nvs_get('val'))+',"msgID":'+str(pycom.nvs_get('msgID'))+'}')
+    wdt.feed()
     machine.pin_deepsleep_wakeup(pins=['P10'], mode=machine.WAKEUP_ANY_HIGH )
     machine.deepsleep(3600000)
